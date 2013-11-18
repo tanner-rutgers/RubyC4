c0b538560b336a75926a024e4bd39c5f731c57d6
amccann <amccann@ualberta.ca>
2013-11-18 10:01:42 -0700

    Added contracts to winCondition and Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,32 @@
 class Player
-	def initialize(name,winCondition,pieces)
-		@pieces = pieces
+  include Test::Unit::TestCase
+
+	def initialize(name,winCondition)
+		assert(@name.is_a?String)
+    assert(winCondition.is_a?WinCondition)
+
 		@name = name
 		@windCondition = winCondition
+
+    assert_equal(@name, name)
+    assert_equal(@windCondition, winCondition)
+
 	end
-	def makeMove(board,move)
-	end
+	def makeMove(board,column)
+
+    # -- Preconditions -- #
+    old_count = board.numTokens(column)
+    assert(!board.nil?)
+    assert(column < board.size[1])
+
+
+    # -- Postconditions -- #
+    assert_equal(board.numTokens(column), old_count+1)
+    assert(board.who(old_count, column), self)
+  end
+
+  def hasWon?(board)
+    assert(!@winCondition.nil?)
+  end
+
 end

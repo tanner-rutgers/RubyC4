10a2cb29e6aa25c19db1255dec9825add7812b72
amccann <amccann@ualberta.ca>
2013-11-22 21:02:37 -0700

    Added headers for game.rb

--- lib/Model/board.rb
+++ lib/Model/board.rb
@@ -46,26 +46,31 @@ module Model
       end
     end
 
+    # Determine who's game peice is at the given x, y location.
     def who(x, y)
+      # -- Pre Conditions -- #
       boardSize = self.size
       assert(x < boardSize[:columns] && y < boardSize[:rows])
       assert(x >= 0 && y >=0)
 
+      # -- Code -- #
       rval = @boardArray[x][y]
 
+      # -- Post Codnitions -- # 
       assert(rval.is_a?(Player) || rval.nil?)
 
       rval
     end
 
+    # Drops the given players peice into the given column.
     def addPiece(columnNumber, player)
       # -- Pre Conditions -- #
       assert(columnNumber >= 0 && columnNumber < @boardArray.size)
       assert(player.is_a?(Player))
       num_tokens_before_insert = numTokens(columnNumber)
       assert(num_tokens_before_insert>=0)
+      
       # -- Code -- #
-
       nextRow = size[:rows] - numTokens(columnNumber) - 1
 
       raise ColumnFullException if numTokens(columnNumber) >= @boardArray[columnNumber].size
@@ -77,6 +82,7 @@ module Model
       assert_equal(num_tokens_before_insert+1, numTokens(columnNumber))
     end
 
+    # Is the board full
     def isFull?
       @boardArray.size.times do |columnNumber|
         return false if numTokens(columnNumber) < @boardArray[columnNumber].size
@@ -88,6 +94,7 @@ module Model
       return { :rows => @boardArray[0].size, :columns => @boardArray.size }
     end
 
+    # Count the number of tokens in the given column.
     def numTokens(columnNumber)
       # -- Pre Conditions -- #
       assert(columnNumber.is_a?Integer)
@@ -103,7 +110,21 @@ module Model
       rval
     end
 
+    # Get  "line" from the given x, y location, in the given x,y direction.
+    # Ex.  A    B    C    0
+    #      D    E    F    0
+    #      G    H    I    0
+    #      0    0    0    0
+    #      getLine(1,1,0,1,2) will return [E,H]
+    #      getLine(1,1,1,0,2) will return [E,F]
+    #      getLine(1,1,-1,-1) will return [E,A]
     def getLine(x, y, x_dir, y_dir, size)
+      assert(x.is_a?Integer)
+      assert(y.is_a?Integer)
+      assert(x_dir.is_a?Integer)
+      assert(y_dir.is_a?Integer)
+      assert(size.is_a?Integer) #win pattern size.
+      
       tokens = []
 
       (size).times do
@@ -117,6 +138,9 @@ module Model
       tokens
     end
 
+    # Get all lines coming from the given x,y location.
+    # @return an array in all directions given the following direction 
+    #         values  [-1, 0, 1] X  [-1, 0, 1], excluding (0,0)
     def getLines(x, y, size)
       lines = []
       [-1, 0, 1].repeated_permutation(2) do |p|

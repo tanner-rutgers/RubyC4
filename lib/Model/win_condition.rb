2f1de6d1816f6f8eb37f9d63a261d62918fbe23b
amccann <amccann@ualberta.ca>
2013-11-24 16:03:05 -0700

    Fixed a WinCondition Error.

--- lib/Model/win_condition.rb
+++ lib/Model/win_condition.rb
@@ -46,36 +46,39 @@ module Model
       string += @pattern.join(", ")
       string + "\n"
     end
+
+    class PatternElement
+
+      def self.ANY
+        PatternElement.new("ANY") {|object| object.is_a?(Model::Player)}
+      end
+
+      def self.OTHER(player)
+        PatternElement.new("OTHER") {|object| object.is_a?(Model::Player) && !object.eql?(player) }
+      end
+
+      def self.PLAYER(player)
+        PatternElement.new("PLAYER") {|object| object.eql?(player)}
+      end
+
+      def match?(object)
+        return @matchBlock.call(object)
+      end
+
+      def to_s
+        "Model::WinCondition:PatternElement.#{@type}"
+      end
+
+      private
+      def initialize (type, &block)
+        @type = type
+        @matchBlock = block
+      end
+
+
+    end
+
   end
 
-  class PatternElement
 
-    def self.ANY
-      PatternElement.new("ANY") {|object| object.is_a?(Model::Player)}
-    end
-
-    def self.OTHER(player)
-      PatternElement.new("OTHER") {|object| object.is_a?(Model::Player) && !object.eql?(player) }
-    end
-
-    def self.PLAYER(player)
-      PatternElement.new("PLAYER") {|object| object.eql?(player)}
-    end
-
-    def match?(object)
-      return @matchBlock.call(object)
-    end
-
-    def to_s
-      "Model::WinCondition:PatternElement.#{@type}"
-    end
-
-    private
-    def initialize (type, &block)
-      @type = type
-      @matchBlock = block
-    end
-
-
-  end
 end

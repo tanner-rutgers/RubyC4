64737a8d21d2cb7a6b6f76bc92065239ee80718f
amccann <amccann@ualberta.ca>
2013-11-24 21:30:51 -0700

    Can now play using the UI.

--- lib/view/ui_game.rb
+++ lib/view/ui_game.rb
@@ -10,6 +10,8 @@ module View
       include UiObserver
       include Test::Unit::Assertions
 
+
+
       def initialize(builder, model)
         # Pre-conditions #
         assert(builder.is_a?(Gtk::Builder), "builder is not a Gtk::Builder")
@@ -21,43 +23,32 @@ module View
         @views = Array.new
 
         @views.push(View::UiBoard.new(@builder, @model.board))
-        #@views.push(View::UiPlayer.new(@builder, @model.players[0]))
-        #@views.push(View::UiOpponent.new(@builder, @model.players[1]))
-
-        @builder.get_object("mainWindow").show()
-        Gtk.main()
+        #@views.push(View::UiStatusInfo.new(@builder, @model))
 
         # Post-conditions / Class-invariants #
         assert_equal(@builder, builder, "GTK builder was not initialized")
         assert_equal(@model, model, "Model was not initialized")
-        assert(!views.nil?)
+        assert(!@views.nil?)
       end
 
       def update
         # Pre-conditions #
         #NA
         @views.each { |view| view.update }
-        #setCurrentTurn
+
         # Post-conditions #
         #NA
       end
-
-      # Display current turn information (label, indicator)
-      def setCurrentTurn    
-        player = @model.currentPlayersTurn
-        colour = player.colour
-        if player==@model.players[0]
-          @builder.get_object('playerTurnImage').set_from_file('./resources/leftArrow_#{colour}.png')
-          @builder.get_object('opponentTurnImage').hide
-          turnLabel = "Your turn!"
-        else
-          @builder.get_object('playerTurnImage').hide
-          @builder.get_object('opponentTurnImage').set_from_file('./resources/rightArrow_#{colour}.png')
-          turnLabel = "Waiting for opponent!"
-        end
-
-        @builder.get_object('currentPlayerLabel').set_text(turnLabel)
+      
+      def get_view(clazz)
+        @views.each {|view| return view if view.class == clazz}
+        return nil
       end
+
+    def show
+        @builder.get_object("mainWindow").show()
+        Gtk.main()
+    end
 
     end
 end

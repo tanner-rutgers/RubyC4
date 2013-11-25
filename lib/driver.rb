64937f6572695ed2f9ade886fae2b884836a72e1
amccann <amccann@ualberta.ca>
2013-11-24 22:39:45 -0700

    New Driver for AI

--- lib/driver.rb
+++ lib/driver.rb
@@ -2,9 +2,11 @@ require_relative 'Controller/game_controller.rb'
 require_relative 'view/ui_game.rb'
 require_relative 'Controller/board.rb'
 require_relative 'Controller/ai.rb'
-#require_relative 'Model/colour.rb'
+require_relative 'Model/colour.rb'
 
 #puts Model::Colour.constants.class
+
+puts Model::Colour::RED
 
 Gtk.init
 @builder = Gtk::Builder::new
@@ -13,14 +15,15 @@ Gtk.init
 yourColourButton = @builder.get_object("yourColourButton")
 yourColourButton.signal_connect( "activate" ) { @builder.get_object('tokenChooserDialog').show }
 
-gameModel = Model::Game.new
+players = [Model::Player.new("Player1", Model::Colour::RED), Model::Player.new("AI-Bob", Model::Colour::BLUE)]
+gameModel = Model::Game.new(players)
 
 game = View::UiGame.new(@builder, gameModel)
 
-boardController = Controller::Board.new(@builder, gameModel,gameModel.players[0])
+boardController = Controller::Board.new(@builder, gameModel, players[0])
 boardController.addObserver(game.get_view(View::UiBoard)) 
 
-aiController = Controller::AI.new(gameModel)
+aiController = Controller::AI.new(players[1], players[0], gameModel)
 aiController.addObserver(game.get_view(View::UiBoard))
 
 boardController.addObserver(aiController)

40328b523c90032413255093a3964912540285d1
jreddeko <jreddeko@ualberta.ca>
2013-11-24 22:16:44 -0700

    whoops,  broken ai

--- lib/Model/ai.rb
+++ lib/Model/ai.rb
@@ -32,15 +32,18 @@ module Model
             begin
                 test.addPiece(x,@player)
                 value = minimax(test,2,false)
-                #puts "best value = #{value} and row = #{x}"
+                #puts "best value = #{bestValue} and row = #{bestMove}"
                 if value > bestValue
                     bestMove = x
                     bestValue = value
+                elsif value == bestValue
+                    bestMove = rand.round == 1 ? bestMove : x
                 end
                 
                 #			puts "mov = #{x} and value = #{value}"
                 rescue
                 #			puts $!
+                
             end
         end
         return bestMove
@@ -49,46 +52,46 @@ module Model
     end
 end
 
-def minimax(board, depth, maximizingPlayer)
-    bestValue = 0
-    newBoard = board
-    if depth == 0 || board.isFull? || @player.hasWon?(board) || @opponent.hasWon?(board)
-        #puts "player.haswon = #{@player.hasWon?(board)} self.hasWon? = #{self.hasWon?(board)}"
-        #puts "depth = #{depth} and board.isFull? = #{board.isFull?}"
-        #puts board
-        return 100*depth + 1 if @player.hasWon?(newBoard) && !maximizingPlayer
-        return -100*depth + 1 if @opponent.hasWon?(newBoard) && maximizingPlayer
-        return 0
-    end
-    if maximizingPlayer
-        bestValue = -100
-        for x in 0..6
-            begin
-                newBoard = board.clone
-                newBoard.addPiece(x,@player)
-                rescue
-            end
-            val = minimax(newBoard,depth-1,false)
-            bestValue = [val,bestValue].max
+    def minimax(board, depth, maximizingPlayer)
+        bestValue = 0
+        newBoard = board
+        if depth == 0 || board.isFull? || @player.hasWon?(board) || @opponent.hasWon?(board)
+            #puts "player.haswon = #{@player.hasWon?(board)} self.hasWon? = #{self.hasWon?(board)}"
+            #puts "depth = #{depth} and board.isFull? = #{board.isFull?}"
+            #puts board
+            return 100*depth + 1 if @player.hasWon?(newBoard) && !maximizingPlayer
+            return -100*depth + 1 if @opponent.hasWon?(newBoard) && maximizingPlayer
+            return 0
         end
+        if maximizingPlayer
+            bestValue = -100
+            for x in 0..6
+                begin
+                    newBoard = board.clone
+                    newBoard.addPiece(x,@player)
+                    rescue
+                end
+                val = minimax(newBoard,depth-1,false)
+                bestValue = [val,bestValue].max
+            end
         #puts "bestValue = #{bestValue}"
-        return bestValue
+            return bestValue
         else
-        bestValue = +100
-        for x in 0..6
-            begin
-                newBoard = board.clone
-                newBoard.addPiece(x,@opponent)
+            bestValue = +100
+            for x in 0..6
+                begin
+                    newBoard = board.clone
+                    newBoard.addPiece(x,@opponent)
                 rescue
+                end
+                val = minimax(newBoard,depth-1,true)
+                bestValue = [val,bestValue].min
             end
-            val = minimax(newBoard,depth-1,true)
-            bestValue = [val,bestValue].min
+
+            return bestValue
         end
-        return bestValue
     end
-end
 def hasWon?(board)
     hasWon = @winCondition.hasWon?(board)
-    
 end
 end

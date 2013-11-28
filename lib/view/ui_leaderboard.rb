98cd023197255413264f7f76a09c0d9a752558ca
Tanner Rutgers <dev.tannner@gmail.com>
2013-11-27 17:14:37 -0700

    Shells for leaderboard and game_list views

---
+++ lib/view/ui_leaderboard.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+require_relative 'ui_observer.rb'
+
+module View
+	class UiLeaderboard
+		include UiObserver
+		include Test::Unit::Assertions
+
+		def initialize
+
+		end
+		
+		def update
+
+		end
+
+	end
+end

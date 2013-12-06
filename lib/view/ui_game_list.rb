1da0ec54a100e8b69028611bf9965bfcb3d3fc85
Tanner Rutgers <dev.tannner@gmail.com>
2013-12-06 15:48:16 -0700

    Games list view completed

--- lib/view/ui_game_list.rb
+++ lib/view/ui_game_list.rb
@@ -1,17 +1,37 @@
 require 'test/unit'
 require_relative 'ui_observer.rb'
+require_relative '../Model/server/client.rb'
 
 module View
 	class UiGameList
 		include UiObserver
 		include Test::Unit::Assertions
 
-		def initialize
+		def initialize(builder, client)
+			# Pre conditions #
+			assert(builder.is_a?(Gtk::Builder), "builder is not a Gtk Builder")
+			assert(client.is_a?(Model::Client), "client is not a Client object")
 
+			@builder = builder
+			@client = client
+
+			# Post conditions #
+			assert_equal(@builder, builder, "builder was not initialized")
+			assert_equal(@client, client, "client was not initialized")
 		end
 
 		def update
+			listStore = @builder.get_object("gamesListStore")
+			listStore.clear
 
+			@client.getGameList.each do |game|
+				entry = listStore.append
+				entry.set_value(0, game.get(:id))
+				entry.set_value(1, game.get(:opponent).name)
+				entry.set_value(2, game.get(:type))
+				entry.set_value(3, game.get(:turn).name)
+				entry.set_value(4, game.get(:timestamp))
+			end
 		end
 
 	end

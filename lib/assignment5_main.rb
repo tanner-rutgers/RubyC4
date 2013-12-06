b89352c4d80aa1f4c19141203626c6b5a32b64b0
Tanner Rutgers <dev.tannner@gmail.com>
2013-12-06 16:22:26 -0700

    Removed game launcher from main file

--- lib/assignment5_main.rb
+++ lib/assignment5_main.rb
@@ -11,7 +11,4 @@ builder.add_from_file(File.expand_path("../C4Ruby.glade", File.dirname(__FILE__)
 clientLogin = Controller::Login.new(builder)
 clientPlayer = clientLogin.client
 
-
-GameLauncher.new(builder, clientPlayer, "Bob", :connect4, 125, false)
-
 Gtk.main()

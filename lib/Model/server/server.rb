dfc8f0f7b9656a6f2fc0f126e0cdb8a07e4090d6
Jason <jreddeko@e5-05-09.ece.ualberta.ca>
2013-12-05 21:48:18 -0700

    server.rb

--- lib/Model/server/server.rb
+++ lib/Model/server/server.rb
@@ -1,47 +1,64 @@
 require 'test/unit'
+require_relative '../database.rb'
 
 module Model
   class Server
 
-    def login(username, password)
-      #preconditions
-      assert(username.is_a?String)
-      assert(password.is_a?String)
-    end
+	def initialize(portno)
+		@db = Database.new
+	end
+    	def login(name,password)
+      		#preconditions
+      		assert(username.is_a?String)
+      		assert(password.is_a?String)
+		return @db.login(name,password)
+    	end
+	# returns an array of new String(usernames)
+    	def get_users()
+		@db.get_users
+   	end
 
-    def board(gameId)
-      assert(gameId.is_a?Integer)
+	# returns the board 
+ 	def get_board(gameId,name,password)
+ 		assert(gameId.is_a?Integer)
+		rval @db.login(name,password) ? @db.get_game(gameId).board : nil
+      		assert(rval.is_a?(Game) || rval.nil?)
+		rval
+	end
 
-      assert(rval.is_a?(Game) || rval.nil?)
-    end
+	# makes move on server, returns nothing
+    	def makeMove(gameId, name, password, move)
+      		assert(gameId.is_a?Integer)
+      		assert(move.is_a?Integer)
+      		assert(user.is_a?User)
+		game_model = @db.login(name,password) ? @db.get_game(gameId) : nil
+		
+		game_model.makeMove(user.player,move)
+		@db.save_game(game_model) 
+		return game_model
+    	end
 
-    def makeMove(gameId, user, move)
-      assert(gameId.is_a?Integer)
-      assert(move.is_a?Integer)
-      assert(user.is_a?User)
+	#
+    	def whosTurn(gameId,name,password)
+      		assert(gameId.is_a?Integer)
+		game_model = @db.login(name,password) ? @db.get_game(gameId) : nil?
+      		return game_model.currentPlayersTurn
+		assert(rval.is_a?User)
+    	end
 
+    	def getGameList(user)
+      		assert(user.is_a?User)
+		return [id, player, turn, timeOfLastMove]  
+      		assert(rval.is_a?Array)
+      		rval.each{|element| assert(element.is_a?(Integer))}
+    	end
 
-    end
+    	def getLeaderboard
 
-    def whosTurn(gameId)
-      assert(gameId.is_a?Integer)
-
-      assert(rval.is_a?User)
-    end
-
-    def getGameList(user)
-      assert(user.is_a?User)
-
-      assert(rval.is_a?Array)
-      rval.each{|element| assert(element.is_a?(Integer))}
-    end
-
-    def getLeaderboard
-
-      #post-conditions
-      assert(rval.is_a?Array)
-      rval.each{|element| assert(element.is_a?(Array) && element.size == 2)}
-    end
+      		#post-conditions
+      		assert(rval.is_a?Array)
+      		rval.each{|element| assert(element.is_a?(Array) && element.size == 2)}
+    	end
 
   end
 end

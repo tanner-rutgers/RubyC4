dbc74ac839d686bafdf8576bd088b7b318d039d1
amccann <amccann@ualberta.ca>
2013-12-06 21:51:47 -0700

    Fixed synch issues

--- lib/Model/server/client.rb
+++ lib/Model/server/client.rb
@@ -23,7 +23,9 @@ module Model
       @username = username
       @password = password
       
-      raise AccessDeniedException if !YAML::load(serverConnection.login(username, password))
+      @instance_lock.synchronize do
+        raise AccessDeniedException if !YAML::load(serverConnection.login(username, password))
+      end
       
       #preconditions
       assert_equal(@username, username)
@@ -32,7 +34,10 @@ module Model
     
     ## ---- Gameplay Commands ---- ##
     def newGame(opponentUsername, gameType)
-      rval = YAML::load(serverConnection.newGame(@username, @password, opponentUsername, gameType))
+      rval = nil
+      @instance_lock.synchronize do
+        rval = YAML::load(serverConnection.newGame(@username, @password, opponentUsername, gameType))
+      end
 
       raise AccessDeniedException if rval == false
       return rval
@@ -43,15 +48,20 @@ module Model
       assert(move.is_a?Integer)
       
       #Make move and raise exception if an credentials are bad.
-      rval =  YAML::load(serverConnection.makeMove(gameId, @username, @password, move))
+      rval = nil
+      @instance_lock.synchronize do
+        rval =  YAML::load(serverConnection.makeMove(gameId, @username, @password, move))
+      end
       raise AccessDeniedException if rval == false
 	
     end
     
     def getBoard(gameId)
       assert(gameId.is_a?Integer)
-            
-      rval = YAML::load(serverConnection.getBoard(gameId, @username, @password))
+      rval = nil
+      @instance_lock.synchronize do      
+        rval = YAML::load(serverConnection.getBoard(gameId, @username, @password))
+      end
       raise AccessDeniedException if rval == false
       
       return rval
@@ -59,8 +69,10 @@ module Model
 
     def whosTurn(gameId)
       assert(gameId.is_a?Integer)
-      
-      rval =  YAML::load(serverConnection.whosTurn(gameId, @username, @password))
+      rval = nil
+      @instance_lock.synchronize do
+        rval =  YAML::load(serverConnection.whosTurn(gameId, @username, @password))
+      end
       raise AccessDeniedException if rval == false
 
       assert(rval.is_a?(Player) || rval.nil?)
@@ -70,8 +82,10 @@ module Model
 
     def getGameType(gameId)
       assert(gameId.is_a?Integer)
-      
-      rval =  YAML::load(serverConnection.getGameType(gameId, @username, @password))
+      rval = nil
+      @instance_lock.synchronize do
+        rval =  YAML::load(serverConnection.getGameType(gameId, @username, @password))
+      end
       raise AccessDeniedException if rval == false
       assert(!rval.nil? && (rval == :connect4 || rval == :otto))
 
@@ -79,15 +93,21 @@ module Model
     end
     
     def getPlayer
-	rval = YAML::load(serverConnection.getPlayer(@username, @password))
-	raise AccessDeniedException if rval == false
+      rval = nil
+      @instance_lock.synchronize do
+	      rval = YAML::load(serverConnection.getPlayer(@username, @password))
+      end
+	    raise AccessDeniedException if rval == false
 	
-	return rval
+	    return rval
     end
 
     def getPlayers(gameId)
       assert(gameId.is_a?Integer)
-      rval =  YAML::load(serverConnection.getPlayers(gameId, @username, @password))
+      rval = nil
+      @instance_lock.synchronize do
+        rval =  YAML::load(serverConnection.getPlayers(gameId, @username, @password))
+      end
       raise AccessDeniedException if rval == false
       
       rval
@@ -95,8 +115,11 @@ module Model
     
     def getWinner(gameId)
       assert(gameId.is_a?Integer)
-      
-      rval =  YAML::load(serverConnection.getWinner(gameId, @username, @password))
+        
+      rval = nil      
+      @instance_lock.synchronize do
+        rval =  YAML::load(serverConnection.getWinner(gameId, @username, @password))
+      end
       raise AccessDeniedException if rval == false
 
       assert(rval.is_a?(Player) || rval.nil?)
@@ -107,8 +130,10 @@ module Model
     
     ## ---- UI Info Commands ---- ##
     def getGameList
-      rval =  YAML::load(serverConnection.getGameList(@username, @password))
-      
+      rval = nil
+      @instance_lock.synchronize do
+        rval =  YAML::load(serverConnection.getGameList(@username, @password))
+      end
       assert(rval.is_a?Array)
       rval.each{|element| assert(element.is_a?(Hash))}
       
@@ -116,7 +141,10 @@ module Model
     end
     
     def getUserList
-      rval =  YAML::load(serverConnection.get_users)
+      rval = nil
+      @instance_lock.synchronize do
+        rval =  YAML::load(serverConnection.get_users)
+      end
       raise AccessDeniedException if rval == false
       
       rval.delete_if {|element| element == @username}
@@ -125,7 +153,10 @@ module Model
     end
 
     def getLeaderboard
-      rval = YAML::load(serverConnection.getLeaderboard)
+      rval = nil
+      @instance_lock.synchronize do
+        rval = YAML::load(serverConnection.getLeaderboard)
+      end
 
       #post-conditions
       assert(rval.is_a?Array)
@@ -135,16 +166,16 @@ module Model
     end
     
     def serverConnection
-      @instance_lock.synchronize do
-        @server = XMLRPC::Client.new("localhost","/RPC2", 50500).proxy("server") if @server.nil?
-        
-        begin
-	        @server.login(@username, @password)
-        rescue Errno::EPIPE => e
-	        puts "Restarting server connection"
-	        @server = XMLRPC::Client.new("localhost","/RPC2", 50500).proxy("server") if @server.nil?
-        end
+      
+      @server = XMLRPC::Client.new("129.128.211.53","/RPC2", 50500).proxy("server") if @server.nil?
+      
+      begin
+        @server.login(@username, @password)
+      rescue Errno::EPIPE => e
+        puts "Restarting server connection"
+        @server = XMLRPC::Client.new("129.128.211.53","/RPC2", 50500).proxy("server") if @server.nil?
       end
+
       
       return @server
     end
